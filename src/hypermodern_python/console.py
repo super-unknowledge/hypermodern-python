33ffdb175973e2a38d6092dc1df25669c98657f1
super-unknowledge <64061479+super-unknowledge@users.noreply.github.com>
2025-03-21 13:09:22 +0800

    add linting tools (local pre-commit hooks not not working)

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -8,20 +8,20 @@ from . import __version__, wikipedia
 
 @click.command()
 @click.option(
-	"--language",
-	"-l",
-	default="en",
-	help="Language edition of Wikipedia",
-	metavar="LANG",
-	show_default=True,
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
 )
 @click.version_option(version=__version__)
 def main(language):
-	"""The hypermodern Python project."""
-	data = wikipedia.random_page(language=language)
+    """The hypermodern Python project."""
+    data = wikipedia.random_page(language=language)
 
-	title = data["title"]
-	extract = data["extract"]
+    title = data["title"]
+    extract = data["extract"]
 
-	click.secho(title, fg="green")
-	click.echo(textwrap.fill(extract))
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
